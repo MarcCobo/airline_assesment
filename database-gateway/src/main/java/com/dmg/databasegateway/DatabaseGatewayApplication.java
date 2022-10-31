81593e34dc8b088f2b36da061e081b3fb3e2d2ef
Kapsaloco <kapsalakosg@gmail.com>
2022-10-31 15:27:29 +0100

    Initial Commit database-gateway

---
+++ database-gateway/src/main/java/com/dmg/databasegateway/DatabaseGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dmg.databasegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseGatewayApplication.class, args);
+	}
+
+}
