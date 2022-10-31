663fdf2bb18a4ea25ff343b35defa14554ce999a
dededec <daviderenacasado@gmail.com>
2022-10-31 16:26:19 +0100

    First batch of reservation tests created.

---
+++ reservations/src/main/java/com/dmg/reservations/controllers/ReservationController.java
@@ -0,0 +1,8 @@
+package com.dmg.reservations.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+
+}
--- reservations/src/test/java/com/dmg/reservations/ReservationsApplicationTests.java
+++ reservations/src/test/java/com/dmg/reservations/ReservationsApplicationTests.java
@@ -1,13 +1,45 @@
 package com.dmg.reservations;
 
+import com.dmg.reservations.models.Reservation;
+import com.dmg.reservations.services.ReservationService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootTest
 class ReservationsApplicationTests {
 
+	@Autowired
+	private ReservationService service;
+
 	@Test
-	void contextLoads() {
+	public void MakeReservation_WhenCalled_ReturnReservation(){
+		Reservation rev = new Reservation(1, 120, "David", "Erena",
+				"Spanish", "12345678A", 23, false);
+		Reservation result = service.save(rev);
+		Assertions.assertNotNull(result);
+	}
+
+	@Test
+	public void FindReservationById_IdExists_ReturnReservation(){
+		Reservation result = service.findById(1);
+		Assertions.assertNotNull(result);
+	}
+
+	@Test
+	public void FindReservationById_IdNotFound_ThrowsException(){
+		Reservation result = service.findById(-1);
+		Assertions.assertNull(result);
+	}
+
+	@Test
+	public void FindReservationByDate_WhenCalled_ReturnList(){
+		List<Reservation> result = service.findByDate(LocalDate.now());
+		Assertions.assertNotNull(result);
 	}
 
 }
