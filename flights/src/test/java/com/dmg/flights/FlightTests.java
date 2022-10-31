d634a5ae2f3c48084fd3f8e3af688b9029e080ea
Cobo <marc.cobo@solera.com>
2022-10-31 16:05:02 +0100

    Added 2 tests to Flights

---
+++ flights/src/test/java/com/dmg/flights/FlightTests.java
@@ -0,0 +1,19 @@
+package com.dmg.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FlightTests {
+
+    @Test
+    public void getFlights_NoFlightsWithDate_MakeCallWithNextDayAndReturnFlights(){
+        /*
+        Get the date for the flights and if that dat doesn't have any flights search the next day
+         */
+    }
+
+    public void getOriginsFromFlight_WhenCalled_ReturnPlaces(){
+
+    }
+}
