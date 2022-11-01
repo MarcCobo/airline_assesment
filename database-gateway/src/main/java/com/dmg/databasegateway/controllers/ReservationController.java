40c7da45ec8d5f89b5db4caf7c3146f04854b575
dededec <daviderenacasado@gmail.com>
2022-11-01 12:57:15 +0100

    Added methods to save Reservation and api-requests file.

--- database-gateway/src/main/java/com/dmg/databasegateway/controllers/ReservationController.java
+++ database-gateway/src/main/java/com/dmg/databasegateway/controllers/ReservationController.java
@@ -1,14 +1,22 @@
 package com.dmg.databasegateway.controllers;
 
+import com.dmg.databasegateway.models.Reservation;
 import com.dmg.databasegateway.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(value="dbapi/reservation")
 public class ReservationController {
 
     @Autowired
     private ReservationService service;
+
+    @PostMapping(path = "/add")
+    public Reservation makeReservation(@RequestBody Reservation reservation){
+        return service.save(reservation);
+    }
 }
--- database-gateway/src/main/java/com/dmg/databasegateway/services/ReservationService.java
+++ database-gateway/src/main/java/com/dmg/databasegateway/services/ReservationService.java
@@ -1,5 +1,6 @@
 package com.dmg.databasegateway.services;
 
+import com.dmg.databasegateway.models.Reservation;
 import com.dmg.databasegateway.repositories.ReservationJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class ReservationService {
 
     @Autowired
     private ReservationJpaRepository repository;
+
+    public Reservation save(Reservation reservation){
+        return repository.save(reservation);
+    }
 }
