e5a922322af1cc2b8551716b3244bc6be9ed0d31
Marc Cobo <marccobo@gmail.com>
2022-11-02 12:07:01 +0100

    Changed data.sql and added dtos

--- database-gateway/src/main/java/com/dmg/databasegateway/controllers/FlightController.java
+++ database-gateway/src/main/java/com/dmg/databasegateway/controllers/FlightController.java
@@ -1,11 +1,15 @@
 package com.dmg.databasegateway.controllers;
 
 import com.dmg.databasegateway.models.Flight;
-import com.dmg.databasegateway.models.Place;
+import com.dmg.databasegateway.models.dto.FlightDTO;
 import com.dmg.databasegateway.services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,15 +19,36 @@ public class FlightController {
     @Autowired
     private FlightService service;
 
+    //Entities
+
     @GetMapping(path = "/getall")
     public List<Flight> getAllFlights() {
         return service.getAllFlights();
     }
 
-
-    @GetMapping(path="/get/{id}")
-    public Flight getFlight(@PathVariable long id){
+    @GetMapping(path = "/get/{id}")
+    public Flight getFlight(@PathVariable long id) {
         return service.getFlight(id);
     }
 
+    //DTOs
+
+    @GetMapping(path = "/getalldto")
+    public List<FlightDTO> getAllFlightsDTO() {
+        List<FlightDTO> flightDTOS = new ArrayList<>();
+        service.getAllFlights().stream().map(flight -> flightDTOS.add(new FlightDTO(flight.getId(), flight.getAirline(),
+                flight.getFlight_num(), flight.getOrigin().getId(),
+                flight.getDestination().getId(), flight.getDate(), flight.getPrice(), flight.getLayover(),
+                flight.getLayover_text(), flight.isLuggage(), flight.getTransit_time())));
+        System.out.println(flightDTOS);
+        return flightDTOS;
+    }
+
+    @GetMapping(path = "/getdto/{id}")
+    public FlightDTO getFlightDTO(@PathVariable long id) {
+        Flight flight = service.getFlight(id);
+        return new FlightDTO(flight.getId(), flight.getAirline(), flight.getFlight_num(), flight.getOrigin().getId(),
+                flight.getDestination().getId(), flight.getDate(), flight.getPrice(), flight.getLayover(),
+                flight.getLayover_text(), flight.isLuggage(), flight.getTransit_time());
+    }
 }
---
+++ database-gateway/src/main/java/com/dmg/databasegateway/models/dto/FlightDTO.java
@@ -0,0 +1,136 @@
+package com.dmg.databasegateway.models.dto;
+
+import java.time.LocalDate;
+
+public class FlightDTO {
+    private long id;
+    private String airline;
+    private String flight_num;
+    private long origin;
+    private long destination;
+    private LocalDate date;
+    private double price;
+    private long layover;
+    private String layover_text;
+    private boolean luggage;
+    private double transit_time;
+
+    public FlightDTO(long id, String airline, String flight_num, long origin, long destination, LocalDate date, double price, long layover, String layover_text, boolean luggage, double transit_time) {
+        this.id = id;
+        this.airline = airline;
+        this.flight_num = flight_num;
+        this.origin = origin;
+        this.destination = destination;
+        this.date = date;
+        this.price = price;
+        this.layover = layover;
+        this.layover_text = layover_text;
+        this.luggage = luggage;
+        this.transit_time = transit_time;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getFlight_num() {
+        return flight_num;
+    }
+
+    public void setFlight_num(String flight_num) {
+        this.flight_num = flight_num;
+    }
+
+    public long getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(long origin) {
+        this.origin = origin;
+    }
+
+    public long getDestination() {
+        return destination;
+    }
+
+    public void setDestination(long destination) {
+        this.destination = destination;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public long getLayover() {
+        return layover;
+    }
+
+    public void setLayover(long layover) {
+        this.layover = layover;
+    }
+
+    public String getLayover_text() {
+        return layover_text;
+    }
+
+    public void setLayover_text(String layover_text) {
+        this.layover_text = layover_text;
+    }
+
+    public boolean isLuggage() {
+        return luggage;
+    }
+
+    public void setLuggage(boolean luggage) {
+        this.luggage = luggage;
+    }
+
+    public double getTransit_time() {
+        return transit_time;
+    }
+
+    public void setTransit_time(double transit_time) {
+        this.transit_time = transit_time;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", airline='" + airline + '\'' +
+                ", flight_num='" + flight_num + '\'' +
+                ", origin=" + origin +
+                ", destination=" + destination +
+                ", date=" + date +
+                ", price=" + price +
+                ", layover=" + layover +
+                ", layover_text='" + layover_text + '\'' +
+                ", luggage=" + luggage +
+                ", transit_time=" + transit_time +
+                '}';
+    }
+}
