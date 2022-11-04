0ba70007dc5d61207e841170b7ebe3e0ee67d4bc
dededec <daviderenacasado@gmail.com>
2022-11-04 11:56:30 +0100

    Analytics created.

--- database-gateway/src/main/java/com/dmg/databasegateway/services/FlightService.java
+++ database-gateway/src/main/java/com/dmg/databasegateway/services/FlightService.java
@@ -1,7 +1,9 @@
 package com.dmg.databasegateway.services;
 
+import com.dmg.databasegateway.models.Analytics;
 import com.dmg.databasegateway.models.Flight;
 import com.dmg.databasegateway.models.Place;
+import com.dmg.databasegateway.repositories.AnalyticsJpaRepository;
 import com.dmg.databasegateway.repositories.FlightJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,8 @@ public class FlightService {
     private FlightJpaRepository repository;
     @Autowired
     private PlaceService placeService;
+    @Autowired
+    private AnalyticsJpaRepository analyticsRepository;
 
     public List<Flight> getAllFlights() {
         return repository.findAll();
@@ -29,22 +33,44 @@ public class FlightService {
 
     public List<Flight> getFlightsFilterByOrigin(String origin) {
         Place place = placeService.findPlaceByName(origin);
+        updateCheckedFlights(place);
         return repository.findFlightByOrigin(place);
     }
 
     public List<Flight> getFlightsFilterByOriginAndDateBetween(String origin, LocalDate startDate, LocalDate endDate) {
         Place place = placeService.findPlaceByName(origin);
+        updateCheckedFlights(place);
         return repository.findFlightByOriginAndDateBetween(place, startDate, endDate);
     }
 
     public List<Flight> getFlightsFilterByOriginAndDestinationAndDateBetween(String origin, String destination, LocalDate startDate, LocalDate endDate) {
         Place place = placeService.findPlaceByName(origin);
+        updateCheckedFlights(place);
         Place place2 = placeService.findPlaceByName(destination);
+        updateCheckedFlights(place2);
         return repository.findFlightByOriginAndDestinationAndDateBetween(place, place2, startDate, endDate);
     }
 
     public List<Flight> getFlightsFilterByOriginAndDate(String origin, LocalDate startDate) {
         Place place = placeService.findPlaceByName(origin);
+        updateCheckedFlights(place);
         return repository.findFlightByOriginAndDate(place, startDate);
+    }
+
+    /*
+     * COMO GESTIONAMOS ESTO CON LOS GET DE VUELOS:
+     *   0.- Creo que antes de acceder hemos encontrado el Place - CIERTO
+     *   1.- analytic = repository.FindByPlace(place); // Tambien con destination
+     *   2.- analytic.checkedFlights++;
+     *   3.- repository.save(analytic);
+     * */
+
+    private void updateCheckedFlights(Place place){
+        Optional<Analytics> optionalAnalytics = analyticsRepository.findById(place.getId());
+        if(optionalAnalytics.isPresent()){
+            Analytics analytics = optionalAnalytics.get();
+            analytics.setCheckedFlights(analytics.getCheckedFlights() +1);
+            analyticsRepository.save(analytics);
+        }
     }
 }
--- database-gateway/src/main/java/com/dmg/databasegateway/services/ReservationService.java
+++ database-gateway/src/main/java/com/dmg/databasegateway/services/ReservationService.java
@@ -1,8 +1,10 @@
 package com.dmg.databasegateway.services;
 
+import com.dmg.databasegateway.models.Analytics;
 import com.dmg.databasegateway.models.Flight;
 import com.dmg.databasegateway.models.Place;
 import com.dmg.databasegateway.models.Reservation;
+import com.dmg.databasegateway.repositories.AnalyticsJpaRepository;
 import com.dmg.databasegateway.repositories.FlightJpaRepository;
 import com.dmg.databasegateway.repositories.PlaceJpaRepository;
 import com.dmg.databasegateway.repositories.ReservationJpaRepository;
@@ -22,8 +24,12 @@ public class ReservationService {
     private FlightJpaRepository flightRepository;
     @Autowired
     private PlaceJpaRepository placeRepository;
+    @Autowired
+    private AnalyticsJpaRepository analyticsRepository;
 
     public Reservation save(Reservation reservation){
+        updateBookedFlights(reservation.getFlightId().getOrigin());
+        updateBookedFlights(reservation.getFlightId().getDestination());
         return repository.save(reservation);
     }
 
@@ -40,9 +46,24 @@ public class ReservationService {
         return repository.save(newReservation);
     }
 
-
     public Reservation getReservation(long id) {
         Optional<Reservation> reservation = repository.findById(id);
         return reservation.get();
+    }
+
+    /*
+     * COMO GESTIONAMOS ESTO CON LAS RESERVAS:
+     *   1.- analytic = repository.FindByPlace(reservation.origin); // Tambien con destination
+     *   2.- analytic.bookedFlights++;
+     *   3.- repository.save(analytic);
+     * */
+
+    private void updateBookedFlights(Place place){
+        Optional<Analytics> optionalAnalytics = analyticsRepository.findById(place.getId());
+        if(optionalAnalytics.isPresent()){
+            Analytics analytics = optionalAnalytics.get();
+            analytics.setBookedFlights(analytics.getBookedFlights() +1);
+            analyticsRepository.save(analytics);
+        }
     }
 }
